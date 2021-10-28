61130c575f52d3ed4fb082ad27ea6947fb6e4d2a
Nux-xader <sumgmng@gmail.com>
2021-10-29 05:41:37 +0700

    initial

---
+++ main.py
@@ -0,0 +1,74 @@
+import sys, os
+
+clr = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+
+def banner():
+	print("""
+ █░█ █▀█ █░░ █▀▄▀█ ▄▀█ █ █░░
+ █▄█ █▀▄ █▄▄ █░▀░█ █▀█ █ █▄▄ V1.0.0
+
+ █▀▀ █▀█ █▀█ █▀▄▀█ ▄▀█ ▀█▀ █▀▀ █▀█
+ █▀░ █▄█ █▀▄ █░▀░█ █▀█ ░█░ ██▄ █▀▄
+
+ Coder by : https://github.com/Nux-xader
+ Contact  : https://wa.me/6281251389915
+ ___________________________________________________________________""")
+
+
+def preperate():
+	while True:
+		try:
+			path = str(input(" File : "))
+			file = open(path, "r").read().split("\n")
+			break
+		except:
+			print(f" [!] File {path} not found")
+
+	while True:
+		saveto = str(input(" Save result to : "))
+		if saveto.split(".")[-1] != "txt": saveto+=".txt"
+		try:
+			open(saveto, "r").read()
+			print(f" File {saveto} already exists")
+		except:
+			break
+
+	return file, saveto
+
+
+def email():
+	data, saveto = preperate()
+
+def url():
+	data, saveto = preperate()
+
+
+def main():
+	while True:
+		clr()
+		banner()
+		print("""
+ [1] Url
+ [2] Email
+ [0] Exit
+ """)
+		while True:
+			choice = str(input(" Choice menu : "))
+			if choice == "1":
+				url()
+				break
+
+			elif choice == "2":
+				email()
+				break
+
+			elif choice == "0":
+				print("\n [+] Program finished")
+				sys.exit()
+
+			else:
+				print(" [!] Invalid choice")
+
+
+if __name__ == '__main__':
+	main()
